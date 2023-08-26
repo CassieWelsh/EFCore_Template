5e9a703f832ec36d944ca8b12c41cad2980b68a5
Cassie Welsh <maksim.airapetyan3@yandex.ru>
2023-08-26 19:31:55 +0300

    Init

--- EFCore_Template/Program.cs
+++ EFCore_Template/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace EFCore_Template
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCore_Template
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                                .AddJsonFile("appsetting.json")
+                                .Build();
+
+            var optBuilder = new DbContextOptionsBuilder<SampleContext>();
+            optBuilder.UseNpgsql(config.GetConnectionString("Postgresql"));
+
+            using var context = new SampleContext(optBuilder.Options);
         }
     }
 }
---
+++ EFCore_Template/SampleContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_Template
+{
+    internal class SampleContext : DbContext
+    {
+        public SampleContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
